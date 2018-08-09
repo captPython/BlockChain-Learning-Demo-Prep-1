b707dd32804bc0e3637e095a9516dc2790e27612
_DevD <10431310+captPython@users.noreply.github.com>
2018-08-09 17:35:39 +0530

    added checks
    modifiers

--- BettingApp/contracts/Betting.sol
+++ BettingApp/contracts/Betting.sol
@@ -29,6 +29,43 @@ struct bid{
 
   }
 
+// C. Modifiers
+// Modifiers validate inputs to functions such as minimal balance or user auth;
+// similar to guard clause in other languages
+// '_' (underscore) often included as last line in body, and indicates
+// function being called should be placed there
+// Example modifier onlyAfter(uint _time) { if (now <= _time) throw; _ }
+
+modifier onlyOwner { 
+     if (msg.sender == owner) 
+       _;
+    }
+
+modifier onlyAfter(uint _time) { if (now <= _time) revert(); _; }
+
+//modifier onlyIfState (state currState) { if (currState != State.A) _; }
+
+// underscore can be included before end of body,
+// but explicitly returning will skip, so use carefully
+modifier checkValue(uint amount) {
+      _;
+  if (msg.value > amount) {
+      uint amountToRefund = amount - msg.value;
+  if (!msg.sender.send(amountToRefund)) {
+       revert();
+    }
+  }
+}
+
+
+// Append right after function declaration
+function changeOwner(address newOwner) onlyAfter(now) onlyOwner()
+        // onlyIfState(State.A)
+{
+owner = newOwner;
+}
+
+
 // Fallback function - Called if other functions don't match call or
 // sent ether without data
 // Typically, called when invalid data is sent
