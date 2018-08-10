e7e1d7119849421300b392e4dd9aaee03f3206ad
_DevD <10431310+captPython@users.noreply.github.com>
2018-08-10 22:48:07 +0530

    commit
    commit

---
+++ StringFunction/contracts/Concatenate.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.22;
+
+
+contract Concatenate {
+  string str1;
+  string str2;
+
+  int8[] array = [int8(1),2,3];
+
+
+  constructor(byte[5][] words) {
+    //  int8[] arrayMemory = array;
+    //string memory word1;
+    //word1 = words[0];
+    //str2 = words[1][];
+  }
+
+
+}
