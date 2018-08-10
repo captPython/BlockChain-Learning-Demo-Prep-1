2d16b78522c3467f5cb687da2dd84d3da4206502
_DevD <10431310+captPython@users.noreply.github.com>
2018-08-10 20:25:32 +0530

    Updates
    Updated functions

--- BettingApp/contracts/Betting.sol
+++ BettingApp/contracts/Betting.sol
@@ -9,23 +9,41 @@ contract Betting {
    // address public gamblerA;
    // address public gamblerB;
 
-    uint[] public outcomes;    // Representing an outcome
+    uint[] public outcomes;             // Representing an outcome
+    uint   public totalBatAmount;
+    uint   public houseBetNumber;            // Betting Counter
 
 /* Structs are custom data structures with self-defined parameters */
     struct Bet {
         uint outcome;
-        uint amount;
+        uint bidAmount;
+        uint winningAmount;
+        uint bidNumber;
         bool initialized;
+    }
+
+    struct GamblerInfo {
+        address gamblerAddress;
+    //    uint bidAmount;
     }
 
 /* Keep track of every gambler's bet */
 	mapping (address => Bet) bets;
 
+/* Bet counter for gamblers */
+  mapping (address => uint) public betCounter;
+    
+/* Keep track of gambler info */  
+  GamblerInfo [] public gamblerInfo;
+
 /* Add any events you think are necessary */
     event BetMade(address gambler);
-    event BetClosed();
+    event BidWinner(address gambler);
+    event NoWinner();
+    event BetClosed(uint bidNumber);
 
 constructor(uint[] _possibleOutcomes) public {
+    houseBetNumber = 1;                    // Initilized the bet number
     owner = msg.sender;
     outcomes = _possibleOutcomes;
    /* for(uint i=0; i < _possibleOutcomes.length; i++){
@@ -43,24 +61,75 @@ constructor(uint[] _possibleOutcomes) public {
     }
 
   /* Gamblers place their bets, preferably after calling checkOutcomes */
-    function makeBet(uint _outcome) public gamblerOnly() payable returns (bool) {
+    function submitBet(uint _outcome) public gamblerOnly() payable returns (bool) {
         require(msg.sender != oracle);
-        require(!bets[msg.sender].initialized); // user cannot double bet
+        require(!bets[msg.sender].initialized);                                // user cannot double bet
+        /** Check if BidClose Event is transmitted */
+        
+        uint winningAmount = bets[gambler].winningAmount;
+                    // Number of bids played by user
+        GamblerInfo memory gamblerObj;
         
         address gambler = msg.sender;
 
-         if (!bets[gambler].initialized) {
-            bets[gambler] = Bet(_outcome, msg.value, true);
-            emit BetMade(gambler);
+    if (!bets[gambler].initialized) {
+       uint bidNumber = bets[gambler].bidNumber + 1;                     // Adding the number of games he / she played
+            bets[gambler] = Bet(_outcome, msg.value, winningAmount,bidNumber, true);
+
+
+            gamblerObj.gamblerAddress = gambler;
+         // gamblerObj.bidAmount = _outcome;
+            gamblerInfo.push(gamblerObj)-1;                             // Indexing the bidders addresess for Loop
+            
+       emit BetMade(gambler);
+            totalBatAmount += msg.value;
             return true;
-        } else {
+    } else {
            return false; // Bets already exist for gambler
-        }
+    }
+  }
+
+  function openBid(uint _outcomeByOracle) public {
+    emit BetClosed(houseBetNumber);                     // No More bets accepted
+    uint loopRun = gamblerInfo.length;
+    uint i = 0;
+    address winner;
+    bool noWinner = true;
+
+    while ( i < loopRun){
+      winner = gamblerInfo[i].gamblerAddress;
+      if (_outcomeByOracle == bets[winner].bidNumber){
+        /* Address is the winner, Add total amount to his winning amount and transfer the amount */
+        /* In the next section, we will give flexibility that use can withdraw funds at the end of the game */
+        bets[winner].winningAmount += totalBatAmount;
+        noWinner = false;
+        emit BidWinner(winner);
+        break;                                            // In Version only one winner is allowed, who made the correct bit 1st
+      }
+      loopRun++;
+    }
+      if(noWinner){
+        /* Trasfer funds to Oracle */
+        emit NoWinner();
+      }
+      houseBetNumber++;                                   // Increment house bet number for next round
+  }
+
+ /* Withdraw the winnings safely (if they have enough) */
+    function withdraw(uint withdrawAmount) public returns (uint) {
+        require(bets[msg.sender].winningAmount >= withdrawAmount);
+           bets[msg.sender].winningAmount -= withdrawAmount;
+        if(!msg.sender.send(withdrawAmount))
+            bets[msg.sender].winningAmount += withdrawAmount;
+
+         return (bets[msg.sender].winningAmount);                   // return the remaining winnings
     }
 
-  function checkBid(uint [] bidArray) public {
 
-  }
+// Append right after function declaration
+function changeOwner(address newOwner) onlyAfter(now) ownerOnly() public {
+    owner = newOwner;
+}
 
 // C. Modifiers
 // Modifiers validate inputs to functions such as minimal balance or user auth;
@@ -73,12 +142,11 @@ constructor(uint[] _possibleOutcomes) public {
   modifier oracleOnly() { if (msg.sender == oracle) _;}
 
   modifier gamblerOnly() { if (!(msg.sender == oracle) || !(msg.sender == owner)) _;}
-  
-  //modifier validBalanceOnly(bidValue) { if (msg.value > bidValue) _;}  // I need to validate this in next level #2
 
   modifier onlyAfter(uint _time) { if (now <= _time) revert(); _; }
 
 // modifier onlyIfState (state currState) { if (currState != State.A) _; }
+//modifier validBalanceOnly(bidValue) { if (msg.value > bidValue) _;}  // I need to validate this in next level #2
 
 // underscore can be included before end of body,
 // but explicitly returning will skip, so use carefully
@@ -92,13 +160,6 @@ modifier checkValue(uint amount) {
   }
 }
 
-
-// Append right after function declaration
-function changeOwner(address newOwner) onlyAfter(now) ownerOnly(){
-    owner = newOwner;
-}
-
-
 // Fallback function - Called if other functions don't match call or
 // sent ether without data
 // Typically, called when invalid data is sent
@@ -107,5 +168,5 @@ function changeOwner(address newOwner) onlyAfter(now) ownerOnly(){
     /* Fallback function */
 function() public payable {
     revert();
-}
+  }
 }
